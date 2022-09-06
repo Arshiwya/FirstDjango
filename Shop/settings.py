ac42f739fb8ad10137b03cd3dc1ef56c01605588
Arshiya Sohrabi Shad <arshiyasohrabi81@gmail.com>
2022-09-07 03:04:35 +0430

    auth-users2

--- Shop/settings.py
+++ Shop/settings.py
@@ -13,8 +13,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+LOGIN_REDIRECT_URL = 'accounts:home'
+LOGIN_URL = 'accounts:login'
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,6 +47,7 @@ INSTALLED_APPS = [
 
 
     'products.apps.ProductsConfig',
+    'accounts.apps.AccountsConfig',
 
 
 ]
@@ -60,7 +67,7 @@ ROOT_URLCONF = 'Shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR ,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from products.models import Product
+
+# Create your views here.
+
+
+@login_required
+def home(request):
+
+	return render(request , 'registration/home.html')
+
+
+
+
+class ProductList(LoginRequiredMixin , ListView):
+
+	queryset = Product.objects.all()
+	template_name = 'registration/home.html'
